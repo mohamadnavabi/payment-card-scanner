d588f34683c582e17a0bf6a630e2a6f32488e929
Mohammad <m.navabifar@sdb247.com>
2022-04-24 19:25:57 +0430

    First commit

---
+++ PaymentCardScanner/src/main/java/ir/mohammadnavabi/paymentcardscanner/CGSize.java
@@ -0,0 +1,12 @@
+package ir.mohammadnavabi.paymentcardscanner;
+
+class CGSize {
+
+	final float width;
+	final float height;
+
+	CGSize(float width, float height) {
+		this.width = width;
+		this.height = height;
+	}
+}
